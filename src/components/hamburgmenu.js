83c821617353173fc4c20c9c40de5ff83c3ea1b3
nikita ananjevs <nikitaananjev@gmail.com>
2020-06-01 20:12:23 +0200

    v.2 Gsap check

--- src/components/hamburgmenu.js
+++ src/components/hamburgmenu.js
@@ -1,19 +1,38 @@
 import React, {useEffect, useRef} from "react"
 import {Link} from "gatsby";
+import gsap from "gsap";
 
+const HamburgerMenu = ({state}) => {
 
-const HamburgerMenu = ({ state }) => {
-
-
+// vARS FOR Animated DOM NODES
     let menu = useRef(null)
+    let revealMenu = useRef(null)
+    let revealMenuBackground = useRef(null)
+    let cityBackground = useRef(null)
+    let line1 = useRef(null)
+    let line2 = useRef(null)
+    let line3 = useRef(null)
+    let info = useRef(null)
+
+
     useEffect(() => {
         if (state.clicked === false) {
             //    Close menu
 
-            menu.style.display = 'none'
+            gsap.to([revealMenu,revealMenuBackground],
+                {
+                    duration:0.8,
+                    height: 0,
+                    ease: "power3.inOut",
+                    stagger:{
+                        amount: 0.07
+                    }
+                });
+
+            // menu.style.display = 'none'
         } else if (state.clicked === true || (state.clicked === true && state.initial === null)) {
             // OPEN menu
-            menu.style.display = 'block'
+            // menu.style.display = 'block'
         }
     })
 
@@ -21,8 +40,8 @@ const HamburgerMenu = ({ state }) => {
     return (
 
         <div ref={el => (menu = el)} className="hamburger-menu">
-            <div className="menu-secondary-background-color"></div>
-            <div className="menu-layer">
+            <div ref={el => (revealMenuBackground = el)} className="menu-secondary-background-color"></div>
+            <div ref={el => (revealMenu = el)} className="menu-layer">
                 <div className="menu-city-background">
 
                 </div>
@@ -31,12 +50,12 @@ const HamburgerMenu = ({ state }) => {
                         <div className="menu-links">
                             <nav>
                                 <ul>
-                                    <li><Link to={`/about`}>About</Link></li>
-                                    <li><Link to={`/work`}>Work</Link></li>
-                                    <li><Link to={`/contact`}>Contact</Link></li>
+                                    <li><Link ref={el => (line1 = el)} to={`/about`}>About</Link></li>
+                                    <li><Link ref={el => (line2 = el)} to={`/work`}>Work</Link></li>
+                                    <li><Link ref={el => (line3 = el)} to={`/contact`}>Contact</Link></li>
                                 </ul>
                             </nav>
-                            <div className="info">
+                            <div ref={el => (info = el)} className="info">
                                 <h3>Our Promiss</h3>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                                     Accusantium beatae, dolorem dolorum in inventore nisi!
